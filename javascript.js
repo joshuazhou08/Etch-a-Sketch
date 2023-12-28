95d36910d33051cbb8f0a27fb083055d09f48bf7
joshuazhou08 <joshuazhou0808@gmail.com>
2023-12-27 16:10:03 -0800

    Add Button For User To Change Canvas Size
    When the button is clicked, it prompts the user to enter a number that
    will be used to determine the canvas size using the
    createGridOffUserInput function, which also deletes the original canvas.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,32 @@
 //Create the grid with nested loops
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
 
-function createGrid(rows, columns){
+function createGrid(rows = 16, columns = 16){
     for (let i = 0; i < rows; i++){
         for (let j = 0; j < columns; j++) {
             const grid = document.createElement('div');
             const generateRandomColor = () => {
-                const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-                grid.style.backgroundColor = randomColor
+                const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);;
+                grid.style.backgroundColor = randomColor;
             };
             grid.setAttribute('class', 'pixel')
-            grid.style.backgroundColor = 'white'
-            grid.addEventListener('mouseenter', generateRandomColor)
-            container.appendChild(grid)
+            grid.style.backgroundColor = 'white';
+            grid.addEventListener('mouseenter', generateRandomColor);
+            let sizeInPixels = 480/rows;
+            grid.style.height = `${sizeInPixels}px`;
+            grid.style.width = `${sizeInPixels}px`;
+            container.appendChild(grid);
         }
     }
 }
-createGrid(16, 16)
+
+function createGridOffUserInput(){
+    const width = prompt("How many pixels wide do you want your canvas to be? Pick a number between 1 and 100.");
+    const oldCanvas = document.querySelectorAll('.pixel');
+    oldCanvas.forEach((element) => element.remove())
+    createGrid(width, width);
+}
+createGrid()
+
+const button = document.querySelector('button')
+button.addEventListener('click', createGridOffUserInput)
